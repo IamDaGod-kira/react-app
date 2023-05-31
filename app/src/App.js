1c6fd115fa322500dbbf030a4e43e1271793098a
Pritam2958 <mantusantra591@gmail.com>
2023-05-31 12:58:26 +0530

    added a build for example

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,15 +8,51 @@ function App() {
         <img align="center" src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <h6 className="bg-gradient-to-tl from-teal-900 to-slate-600 capitalize italic p-5">Your contribution is a necessity!</h6>
         </p>
-      <p className="capitalize italic text-xl text-center p-4 text-slate-900 bg-gradient-to-br from-yellow-600 to-orange-600">
-        Et veniam dolor mollit ipsum nisi. Cillum ad ut elit consectetur cillum commodo reprehenderit sit non. Qui dolor amet ut ex incididunt aute eu. Deserunt adipisicing pariatur laboris non labore. Ipsum commodo ullamco ea in dolor. Nulla ullamco cillum nisi laborum velit voluptate fugiat qui ullamco exercitation proident ex duis magna. Qui magna mollit deserunt laborum incididunt.
-        Occaecat magna adipisicing in esse deserunt laboris tempor. Ut nulla proident est cillum labore ipsum ea consectetur voluptate duis quis. Qui et excepteur incididunt quis in ut amet ex quis. Non voluptate proident ipsum dolore qui voluptate ad eu duis ullamco.
-        Laboris sit anim aliquip ullamco ea est culpa. Aute sunt nulla sit exercitation dolore aliqua dolore occaecat occaecat occaecat est ipsum ullamco. Sit nisi do incididunt officia.
-        Lorem pariatur reprehenderit amet irure labore quis occaecat ipsum et. Amet excepteur velit quis est ex deserunt laboris irure in laborum exercitation. Duis aute nisi ea nisi id eiusmod aliquip pariatur duis consequat. Officia veniam irure aliquip ex eu amet irure. Deserunt aliquip incididunt nostrud sint eiusmod esse.
-        Reprehenderit ullamco esse incididunt enim. Aliquip incididunt anim magna laborum sint deserunt proident excepteur. Labore ullamco adipisicing in sit commodo velit tempor in ex officia esse. Tempor enim nisi deserunt mollit incididunt do officia est veniam nisi. Reprehenderit ea veniam cupidatat id qui minim proident fugiat. Nulla do sint consequat culpa reprehenderit adipisicing laboris esse officia consectetur elit eiusmod. Irure voluptate commodo qui pariatur ut culpa nostrud eiusmod.
-      </p>
+        <br />
+        <h5 className="capitalize font-serif italic">
+          How to Add your contribution:
+        </h5>
+        <br />
+        <p className="uppercase font-extrabold italic">
+          You will find the Repo{" "}
+          <a
+            className="text-sm text-amber-700 underline"
+            href="https://github.com/Pritam2958/react-app"
+          >
+            here
+          </a>
+        </p>
+        <h6 className="bg-gradient-to-tl from-teal-900 to-slate-600 capitalize italic p-5">
+          Your contribution is a necessity!
+        </h6>
+        <p className="capitalize italic text-xl text-center p-4 text-slate-900 bg-gradient-to-br from-yellow-600 to-orange-600">
+          Et veniam dolor mollit ipsum nisi. Cillum ad ut elit consectetur
+          cillum commodo reprehenderit sit non. Qui dolor amet ut ex incididunt
+          aute eu. Deserunt adipisicing pariatur laboris non labore. Ipsum
+          commodo ullamco ea in dolor. Nulla ullamco cillum nisi laborum velit
+          voluptate fugiat qui ullamco exercitation proident ex duis magna. Qui
+          magna mollit deserunt laborum incididunt. Occaecat magna adipisicing
+          in esse deserunt laboris tempor. Ut nulla proident est cillum labore
+          ipsum ea consectetur voluptate duis quis. Qui et excepteur incididunt
+          quis in ut amet ex quis. Non voluptate proident ipsum dolore qui
+          voluptate ad eu duis ullamco. Laboris sit anim aliquip ullamco ea est
+          culpa. Aute sunt nulla sit exercitation dolore aliqua dolore occaecat
+          occaecat occaecat est ipsum ullamco. Sit nisi do incididunt officia.
+          Lorem pariatur reprehenderit amet irure labore quis occaecat ipsum et.
+          Amet excepteur velit quis est ex deserunt laboris irure in laborum
+          exercitation. Duis aute nisi ea nisi id eiusmod aliquip pariatur duis
+          consequat. Officia veniam irure aliquip ex eu amet irure. Deserunt
+          aliquip incididunt nostrud sint eiusmod esse. Reprehenderit ullamco
+          esse incididunt enim. Aliquip incididunt anim magna laborum sint
+          deserunt proident excepteur. Labore ullamco adipisicing in sit commodo
+          velit tempor in ex officia esse. Tempor enim nisi deserunt mollit
+          incididunt do officia est veniam nisi. Reprehenderit ea veniam
+          cupidatat id qui minim proident fugiat. Nulla do sint consequat culpa
+          reprehenderit adipisicing laboris esse officia consectetur elit
+          eiusmod. Irure voluptate commodo qui pariatur ut culpa nostrud
+          eiusmod.
+        </p>
       </header>
     </div>
   );
